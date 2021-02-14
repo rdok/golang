d4c81e90cb27182a75a73d69147f94d84b3c520f
Rizart Dokollari <r.dokollari@gmail.com>
2021-02-14 17:11:52 +0000

    Fetch a URL content.

---
+++ 01-tutorial/1.5-fetching-a-url/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	urls := [2]string{"https://rdok.co.uk/", "https://github.com/rdok"}
+
+	for _, url := range urls {
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		body, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", body)
+	}
+}
